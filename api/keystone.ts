c5a6ab4aa38f61b1752f415047d73ab8c9592802
RJGJ <jaictinrj@gmail.com>
2026-07-18 15:52:20 +0800

    initialized the keystone api

---
+++ api/keystone.ts
@@ -0,0 +1,38 @@
+// Welcome to Keystone!
+//
+// This file is what Keystone uses as the entry-point to your headless backend
+//
+// Keystone imports the default export of this file, expecting a Keystone configuration object
+//   you can find out more at https://keystonejs.com/docs/apis/config
+
+import { config } from '@keystone-6/core'
+
+// to keep this file tidy, we define our schema in a different file
+import { lists } from './schema'
+
+// authentication is configured separately here too, but you might move this elsewhere
+// when you write your list-level access control functions, as they typically rely on session data
+import { withAuth, session } from './auth'
+
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+
+export default withAuth(
+  config({
+    server: {
+      port: Number.parseInt(process.env.APP_PORT || '3000'),
+    },
+    db: {
+      provider: 'postgresql',
+      url: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+      onConnect: async (context) => {
+        /* ... */
+      },
+      // Optional advanced configuration
+      enableLogging: true,
+      idField: { kind: 'uuid' },
+    },
+    lists,
+    session,
+  }),
+)
