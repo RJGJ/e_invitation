d82524ff039a1f5134342a218e134fbef8dd3192
RJGJ <jaictinrj@gmail.com>
2026-07-19 09:30:35 +0800

    feat: add radius and spacing tokens

---
+++ app/lib/theme/app_spacing.dart
@@ -0,0 +1,7 @@
+abstract final class AppSpacing {
+  static const double unit = 8;
+  static const double gutter = 24;
+  static const double marginMobile = 20;
+  static const double marginDesktop = 64;
+  static const double containerMax = 1200;
+}
