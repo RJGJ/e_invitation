9fc6c1913d68df338b004f6c54032fe98b643395
RJGJ <jaictinrj@gmail.com>
2026-08-15 18:23:11 +0800

    fix: set is_superuser in UserManager.create_superuser

--- api/users/models.py
+++ api/users/models.py
@@ -25,6 +25,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, name, password=None, **extra_fields):
         extra_fields.setdefault("is_admin", True)
         extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
         return self._create_user(email, name, password, **extra_fields)
 
 
